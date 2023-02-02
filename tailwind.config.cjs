1f4821153d83db83e2d9efaa70cb26234558feef
Ahmad Zain AF <zainazharul@gmail.com>
2023-02-02 19:01:57 +0700

    chore: turned off daisy ui default theme, added custom color, added server host

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: "#0F4C75",
+      },
+    },
   },
   plugins: [require("daisyui")],
-}
+  daisyui: {
+    themes: false,
+  },
+};
