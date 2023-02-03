e56c2d469ffa68f0cd45a0f0ec0f196ae21f777d
Ahmad Zain AF <zainazharul@gmail.com>
2023-02-03 20:13:30 +0700

    feat: created conditions to hide admin menus from user

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,6 +9,7 @@ import {
   TbSettings,
 } from "react-icons/tb";
 import React, { FC, LabelHTMLAttributes } from "react";
+import { useCookies } from "react-cookie";
 
 interface SidebarProps extends LabelHTMLAttributes<HTMLParagraphElement> {
   homeSet?: string;
@@ -29,6 +30,10 @@ const Sidebar: FC<SidebarProps> = ({
   inboxSet,
   settingsSet,
 }) => {
+  const [cookie] = useCookies(["role"]);
+  const checkRole = cookie.role;
+  const admin = checkRole == "admin";
+
   return (
     <div className="bg-navy h-screen w-52 shadow-lg sticky top-0 z-40 flex pt-28 pb-16 flex-col justify-between items-center">
       <section className="w-full flex flex-col font-semibold text-white gap-2">
@@ -44,12 +49,14 @@ const Sidebar: FC<SidebarProps> = ({
             Profile
           </div>
         </p>
-        <p className={`px-7 py-2 ${employeesSet}`}>
-          <div className="flex gap-2 items-center duration-300 hover:text-black/50 hover:cursor-pointer active:scale-90">
-            <TbUsers size={25} />
-            Employees
-          </div>
-        </p>
+        {admin && (
+          <p className={`px-7 py-2 ${employeesSet}`}>
+            <div className="flex gap-2 items-center duration-300 hover:text-black/50 hover:cursor-pointer active:scale-90">
+              <TbUsers size={25} />
+              Employees
+            </div>
+          </p>
+        )}
         <p className={`px-7 py-2 ${recordsSet}`}>
           <div className="flex gap-2 items-center duration-300 hover:text-black/50 hover:cursor-pointer active:scale-90">
             <TbAddressBook size={25} />
@@ -68,12 +75,14 @@ const Sidebar: FC<SidebarProps> = ({
             Inbox
           </div>
         </p>
-        <p className={`px-7 py-2 ${settingsSet}`}>
-          <div className="flex gap-2 items-center duration-300 hover:text-black/50 hover:cursor-pointer active:scale-90">
-            <TbSettings size={25} />
-            Settings
-          </div>
-        </p>
+        {admin && (
+          <p className={`px-7 py-2 ${settingsSet}`}>
+            <div className="flex gap-2 items-center duration-300 hover:text-black/50 hover:cursor-pointer active:scale-90">
+              <TbSettings size={25} />
+              Settings
+            </div>
+          </p>
+        )}
       </section>
       <section>
         <div className="flex justify-center items-center mx-7 px-5 py-3 border-2 border-white rounded-xl shadow-md shadow-black gap-1 font-bold text-yellow-400 duration-300 hover:cursor-pointer active:scale-90">
