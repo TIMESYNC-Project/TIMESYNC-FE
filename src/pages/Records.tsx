a502223e995ab64b7c1fe78983d45f369f507da5
aryoyudhanto <aryoyudhanto26399@gmail.com>
2023-02-09 00:51:33 +0700

    feat: integration records page admin

--- src/pages/Records.tsx
+++ src/pages/Records.tsx
@@ -2,20 +2,64 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { BsSearch } from "react-icons/all";
 import { useCookies } from "react-cookie";
+import axios from "axios";
 
 import { CustomInput } from "components/CustomInput";
 import { WrappingCard } from "components/Card";
 import { FlexyCard } from "components/Card";
 import Layout from "components/Layout";
 
+interface EmployeesType {
+  id: number;
+  name: string;
+  nip: string;
+  position: string;
+  profile_picture: string;
+}
+
 const Records = () => {
+  const [employees, setEmployees] = useState<EmployeesType[]>([]);
+  const [search, setSearch] = useState<string>("");
   const [cookie, setCookie] = useCookies();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getEmployees();
+  }, [search]);
+
+  function getEmployees() {
+    axios
+      .get(`employees`, {
+        headers: {
+          Authorization: `Bearer ${cookie.token}`,
+        },
+      })
+      .then((res) => {
+        const { data } = res.data;
+        setEmployees(data);
+      })
+      .catch((err) => {});
+  }
+
+  function searchEmployees(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    axios
+      .get(`search?q=${search}`, {
+        headers: {
+          Authorization: `Bearer ${cookie.token}`,
+        },
+      })
+      .then((res) => {
+        const { data } = res.data;
+        setEmployees(data);
+      })
+      .catch((err) => {});
+  }
 
   function onClickDetail(id: number) {
     navigate(`/records/details/${id}`);
   }
-  
+
   return (
     <Layout recordsSet="w-full bg-gradient-to-r from-white to-navy hover:text-white">
       {cookie.role === "admin" ? (
@@ -23,36 +67,56 @@ const Records = () => {
           judul="Records"
           rightSide={
             <div className="flex justify-end">
-              <form className="flex justify-end" id="form-search">
-                <CustomInput inputSet="border-sky" placeholder="Search" id="input-search"/>
-                <button className="btn btn-ghost mx-1 text-sky" type="submit" id="btn-search"> 
+              <form className="flex justify-end" id="form-search" onSubmit={searchEmployees}>
+                <CustomInput
+                  inputSet="border-sky"
+                  placeholder="Search"
+                  id="input-search"
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <button
+                  className="btn btn-ghost mx-1 text-sky"
+                  type="submit"
+                  id="btn-search"
+                >
                   <BsSearch size={27} />
                 </button>
               </form>
             </div>
           }
         >
-          <FlexyCard parentSet="active:scale-95">
-            <div className="flex justify-center items-center hover:cursor-pointer " onClick={()=>onClickDetail(2)} id="btn-detail-records">
-              <div className="flex w-1/2">
-                <img
-                  src="https://i.pinimg.com/564x/9f/8b/74/9f8b749c32edf47b1b3f098230a5584c.jpg"
-                  className="w-[50px] h-[50px]  rounded-full"
-                />
-                <div className="mx-7">
-                  <p className="font-medium text-lg text-navy">0001</p>
-                  <p className="font-bold text-lg text-navy">James Shelby</p>
+          {employees.map((data) => {
+            return (
+              <FlexyCard parentSet="active:scale-95">
+                <div
+                  key={data.id}
+                  className="flex justify-center items-center hover:cursor-pointer "
+                  onClick={() => onClickDetail(2)}
+                  id="btn-detail-records"
+                >
+                  <div className="flex w-1/2">
+                    <img
+                      src={data.profile_picture}
+                      className="w-[50px] h-[50px]  rounded-full"
+                    />
+                    <div className="mx-7">
+                      <p className="font-medium text-lg text-navy">{data.nip}</p>
+                      <p className="font-bold text-lg text-navy">
+                        {data.name}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex w-1/2 justify-end">
+                    <div className="mx-5">
+                      <p className="font-bold text-lg text-navy">
+                        {data.position}
+                      </p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="flex w-1/2 justify-end">
-                <div className="mx-5">
-                  <p className="font-bold text-lg text-navy">
-                    Product Engineer
-                  </p>
-                </div>
-              </div>
-            </div>
-          </FlexyCard>
+              </FlexyCard>
+            );
+          })}
         </WrappingCard>
       ) : (
         <WrappingCard
@@ -61,7 +125,7 @@ const Records = () => {
             <>
               <div className="flex justify-center items-center">
                 <CustomInput
-                id="input-date"
+                  id="input-date"
                   type="date"
                   inputSet="border-sky"
                   // onChange={}
