4319f67381a9b42bf38d896130232e1344e357ff
Ahmad Zain AF <zainazharul@gmail.com>
2023-02-09 14:17:48 +0700

    feat: modified card component and implemented responsive design on company profile page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,6 +11,7 @@ interface CardProps {
 }
 
 export const WrappingCard: FC<CardProps> = ({
+  titleSet,
   judul,
   parentSet,
   children,
@@ -20,10 +21,14 @@ export const WrappingCard: FC<CardProps> = ({
     <div className="my-10 mx-5 md:mx-12 xl:m-20">
       <div className="box-border w-full bg-white rounded-3xl border-sky border-2">
         <div className="flex mx-3 md:mx-5 xl:mx-10 mt-10 items-center">
-          <div className="w-1/2">
-            <h1 className="capitalize text-3xl font-extrabold">{judul}</h1>
+          <div className="w-2/3">
+            <h1
+              className={`capitalize text-xl md:text-2xl lg:text-3xl font-extrabold ${titleSet}`}
+            >
+              {judul}
+            </h1>
           </div>
-          <div className="w-1/2 flex justify-end">{rightSide}</div>
+          <div className="w-1/3 flex justify-end">{rightSide}</div>
         </div>
         <hr className="mx-3 md:mx-5 xl:mx-10 my-3 border-[1.5px] border-sky" />
         <div className={`${parentSet} p-3 md:p-6 xl:p-10`}>{children}</div>
